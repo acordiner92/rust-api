95c96017c7bdc9b48976d7cb7f176d584c08649a
Andrew Cordiner <andrewcordiner92@gmail.com>
2023-11-28 00:06:30 +1100

    Created hello world axum api server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(hello_world));
+    let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
+
+    axum::serve(listener, app).await.unwrap()
+}
+
+async fn hello_world() -> String {
+    "Hello World!!".to_string()
 }
