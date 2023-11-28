ce85368f0c6556f275fde8d1bc57530e5a4d27fe
Andrew Cordiner <andrewcordiner92@gmail.com>
2023-11-28 15:00:23 +1100

    Added mirror_body_json endpoint

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@ use tokio::net::TcpListener;
 
 pub async fn run() {
     let app = create_routes();
-    let listener: TcpListener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
 
     axum::serve(listener, app).await.unwrap()
 }
