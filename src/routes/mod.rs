5290d2db53e9eb38c6b0199420fcb31da1780318
Andrew Cordiner <andrewcordiner92@gmail.com>
2023-11-28 16:02:25 +1100

    Added cors

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -6,6 +6,7 @@ mod path_variables;
 mod query_params;
 
 use axum::{
+    http::Method,
     routing::{get, post},
     Router,
 };
@@ -15,8 +16,13 @@ use mirror_body_string::mirror_body_string;
 use mirror_user_agent::mirror_user_agent;
 use path_variables::{hard_coded_path, path_variables};
 use query_params::query_params;
+use tower_http::cors::{Any, CorsLayer};
 
 pub fn create_routes() -> Router {
+    let cors = CorsLayer::new()
+        .allow_methods([Method::GET, Method::POST])
+        .allow_origin(Any);
+
     Router::new()
         .route("/", get(hello_world))
         .route("/mirror_body_string", post(mirror_body_string))
@@ -25,4 +31,5 @@ pub fn create_routes() -> Router {
         .route("/path_variables/:id", get(path_variables))
         .route("/query_params", get(query_params))
         .route("/mirror_user_agent", get(mirror_user_agent))
+        .layer(cors)
 }
